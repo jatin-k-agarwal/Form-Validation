bb77c00d3863691d10bcb1f02d4ac4704e756696
Jatin Agarwal <jatin.kagarwal@gmail.com>
2025-06-08 15:19:58 +0530

    Set up GitHub Pages deployment using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import FormPage from './FormPage';
 import SuccessPage from './SuccessPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-        <Route path="/success" element={<SuccessPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<FormPage />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
   );
 }
 
